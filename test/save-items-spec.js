ce6c24e548d2659758974150534fea2374c014d7
Cub Olson <cub.olson@gmail.com>
2020-12-11 18:37:36 -0500

    Final push

--- test/save-items-spec.js
+++ test/save-items-spec.js
@@ -2,10 +2,20 @@ const { expect } = require('chai');
 const { saveItems } = require('../save-items');
 describe("The saveItems function", () => {
   it('adds the new item to the list', () => {
-    expect.fail('please write this test');
+    const items = ['1', '2'];
+    const newItem = '3';
+
+    const result = saveItems(items, newItem);
+
+    expect(result).to.contain(newItem);
   });
 
   it('makes sure the result and the original are different', () => {
-    expect.fail('please write this test');
+    const items = ['1', '2'];
+    const newItem = '3';
+
+    const result = saveItems(items, newItem);
+
+    expect(items === result).to.be.false;
   });
 });
